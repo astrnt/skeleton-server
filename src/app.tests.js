3ee6aa8547e7189a9642129b2ded2f310bd02cb8
Arie M. Prasetyo <ari.setyo@gmail.com>
2020-11-11 22:48:38 +0700

    setup test environment

---
+++ src/app.tests.js
@@ -0,0 +1,19 @@
+/**
+ * Test file for the main router app
+ * @author Arie M. Prasetyo (2020)
+ */
+
+import {assert} from 'chai';
+
+const {equal} = assert;
+
+/**
+ * Tests for each endpoint
+ */
+describe('Tests for main API router', () => {
+
+  it('should return a correct `server` GET request payload', () => {
+    equal(true, true);
+  });
+
+});
